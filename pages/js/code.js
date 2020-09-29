487ef066c7773cbfd0d9d0c929351dd1c92a1cde
Caio <caiobernardodasilva@gmail.com>
2020-09-29 03:59:39 -0300

    Task completed

---
+++ pages/js/code.js
@@ -0,0 +1,24 @@
+const search = async () => {
+    return await fetch('https://api.github.com/users/zcaiobs/repos').then(res => res.json())
+        .catch(err => console.log('Erro', err));
+}
+
+const elemento = document.getElementById('repos');
+
+search().then(res => {
+    res.map(item => {
+        let date = new Date(item.created_at);
+        elemento.insertAdjacentHTML('beforeend', `
+        <div class="repos"><h1>Project: ${item.name}</h1>
+        <li>Language: ${item.language}</li>
+        <li>★: ${item.forks}</li>
+        <li>Created: ${date.getDate()}/${date.getMonth() + 1 < 10 ?
+                '0' + (date.getMonth() + 1)
+                : date.getMonth() + 1}/${+date.getFullYear()}</li>
+        <li>Repository: <a href="${item.svn_url}">zcaiobs/${item.name}</a></li></div>
+        `);
+
+    });
+});
+
+
